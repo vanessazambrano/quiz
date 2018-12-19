5ea226c6b7870ec5b757ba67e816c3f26871b5ef
Emily Garvey <41160828+emilygarvey@users.noreply.github.com>
2018-12-19 18:14:33 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@
 $(document).ready(function() {
     $("button").click(function() {
         var name = $("#name").val();
-        var techPlacement = "TBD";
+        var finalAnswer = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Result = $("#question1").val();
-        var q2Result = $("#question2").val();
-        var q3Result = $("#question3").val();
+        var q1Input = $("#question1").val();
+        var q2Input = $("#question2").val();
+        var q3Input = $("#question3").val();
         var totalScore;
     });
 
